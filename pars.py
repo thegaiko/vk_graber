1b1823f9da3dd4390a18129a0c637d0ce936f9f6
Doondand <gevor.02@gmail.com>
2022-03-03 02:50:31 +0300

    refactor

--- pars.py
+++ pars.py
@@ -7,28 +7,31 @@ from send import post_check
 
 
 names = ['pppppp_pppp_p', 'nnrv0', 'public69767901',
-         'thankunext17', 'bonjourlasolitude', 'blooossom']
-offsets = [1, 1, 1, 1, 1, 1]
-counts = [2, 2, 2, 2, 2, 2]
+         'thankunext17', 'purplecherrybot', 'blooossom']
+offsets = [1, 1, 1, 1, 1, 0]
+counts = [2, 2, 2, 2, 2, 1]
+timers = [1, 2, 3, 4, 5, 6]
 
 
-def post_send(name, offset, count):
-    id = post_check(name, offset, count)[0]
-    post_text = post_check(name, offset, count)[1]
-
-    old_id = id
+def post_send(name, offset, count, timer):
+    old_id = post_check(name, offset, count)[0]
     while True:
-        post_check(name, offset, count)
+        id = post_check(name, offset, count)[0]
         if old_id < id:
             if len(post_check(name, offset, count)) == 3:
+                post_text = post_check(name, offset, count)[1]
                 photo_id = post_check(name, offset, count)[2]
                 post_photo(post_text, photo_id)
             else:
+                post_text = post_check(name, offset, count)[1]
                 post(post_text)
+            old_id = id
+        time.sleep(timer)
 
 
 t = []
 
 for i in range(len(names)):
-    t.append(Thread(target=post_send, args=(names[i], offsets[i], counts[i],)))
+    t.append(Thread(target=post_send, args=(
+        names[i], offsets[i], counts[i], timers[i])))
     t[i].start()
--- posting.py
+++ posting.py
@@ -1,12 +1,10 @@
 import requests
 
-owner_id = '210983267'  # id of our group
-
 
 def post(post_text):
     url = "https://api.vk.com/method/wall.post"
 
-    payload = f'owner_id=-{owner_id}&friends_only=0&from_group=0&message={post_text}&publish_date=1677761844&signed=0&mark_as_ads=0&close_comments=0&mute_notifications=0&access_token=f731ac8724793016d8e04ae80823babaa2b9e8d4915c921deab5f0b190b48b0a193ec6ab0a1eb200c816b&v=5.131'
+    payload = f'owner_id=-210983267&friends_only=0&from_group=0&message={post_text}&publish_date=1677761844&signed=0&mark_as_ads=0&close_comments=0&mute_notifications=0&access_token=f731ac8724793016d8e04ae80823babaa2b9e8d4915c921deab5f0b190b48b0a193ec6ab0a1eb200c816b&v=5.131'
     headers = {
         'authority': 'api.vk.com',
         'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
@@ -22,13 +20,13 @@ def post(post_text):
         'referer': 'https://dev.vk.com/',
         'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
     }
-    response = requests.request("POST", url, headers=headers, params=payload)
+    requests.request("POST", url, headers=headers, params=payload)
 
 
 def post_photo(post_text, photo_id):
     url = "https://api.vk.com/method/wall.post"
 
-    payload = f'owner_id=-{owner_id}&friends_only=0&from_group=0&message={post_text}&publish_date=1677761844&attachments={photo_id}&signed=0&mark_as_ads=0&close_comments=0&mute_notifications=0&access_token=f731ac8724793016d8e04ae80823babaa2b9e8d4915c921deab5f0b190b48b0a193ec6ab0a1eb200c816b&v=5.131'
+    payload = f'owner_id=-210983267&friends_only=0&from_group=0&message={post_text}&publish_date=1677761844&attachments={photo_id}&signed=0&mark_as_ads=0&close_comments=0&mute_notifications=0&access_token=f731ac8724793016d8e04ae80823babaa2b9e8d4915c921deab5f0b190b48b0a193ec6ab0a1eb200c816b&v=5.131'
     headers = {
         'authority': 'api.vk.com',
         'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="98", "Google Chrome";v="98"',
@@ -45,4 +43,4 @@ def post_photo(post_text, photo_id):
         'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7'
     }
 
-    response = requests.request("POST", url, headers=headers, params=payload)
+    requests.request("POST", url, headers=headers, params=payload)
